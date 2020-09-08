209c7c39c3ab96d2bf7951b9661b509c08e2e473
Seekaplayer <seekaplayer@pop-os.localdomain>
2020-09-07 21:00:43 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+# imports
+import random
+
+# this function creates a random number based on the two numbers the user chose
+def random_numbers(min_num, max_num):
+    return random.randint(min_num, max_num)
+
+
+# this function checks to see if the input is a number
+def is_a_number(msg):
+    user = input(msg)
+    while not user.isdigit():
+        user = input(msg)
+    return int(user)
+
+
+# this function checks to see if the second number is lower then the first number
+def is_num_less(user, value):
+    while user <= value:
+        return is_a_number("Choose a higher number: ")
+
+
+def restart_game():
+    choice = input("You only have two choices! P or Q: ")
+    while choice != "p" and choice != "q":
+        choice = input("You only have two choices! p or q!: ")
+    return choice == "p"
+
+
+# this function starts the game
+def start_game(game):
+
+    while game:
+        # gets the user inputs
+        user_min_num = is_a_number("Please provide a low number: ")
+        user_max_num = is_a_number("Please Provide a high number: ")
+
+        # check if the high number is lower then the lower number
+        while user_max_num <= user_min_num:
+            user_max_num = is_num_less(user_max_num, user_min_num)
+
+        # generates a random number
+        random_number = random_numbers(user_min_num, user_max_num)
+
+        # the user guesses the random number that was generated
+        guess = is_a_number("Guess the number that was generated: ")
+
+        while guess != random_number:
+            if guess < random_number:
+                guess = is_a_number("That choice is to low: ")
+            else:
+                guess = is_a_number("That was choice was to high: ")
+
+        print(f"Congratulations your choice of {guess} was correct!")
+
+        game = restart_game()
+
+
+# starts the game when the script is loaded
+start_game(True)
